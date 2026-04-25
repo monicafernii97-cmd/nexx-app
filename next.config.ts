0495c0b43cf54c51c12f251edf66fa4b6d569b88
Monica Fernandez <monicafernii97@gmail.com>
2026-04-25 11:47:31 -0500

    fix: narrow next.config image allowlist to img.clerk.com only

--- next.config.ts
+++ next.config.ts
@@ -18,8 +18,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'img.clerk.com' },
-      { protocol: 'https', hostname: '*.clerk.com' },
-      { protocol: 'https', hostname: '*.clerkusercontent.com' },
     ],
   },
 };
